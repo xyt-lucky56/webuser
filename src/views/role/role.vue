7b9084763e889794d9a930ab3f4cb8e57c128576
wmj <wumengjun@lucky56.com.cn>
2019-09-04 10:31:18 +0800

    角色开始

--- src/views/role/role.vue
+++ src/views/role/role.vue
@@ -1,5 +1,17 @@
 <template>
-    <div>
-        角色
+    <div class="role">
+        <h1>角色</h1>       
     </div>
 </template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="less" scoped>
+.role {
+    h1 {
+        margin-top: 50px; 
+    }
+}
+</style>
